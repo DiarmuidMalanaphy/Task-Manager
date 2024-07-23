fd0798f1364dfff165013de11e01309d3a25017a
diarmuidmalanaphy <diarmuidmalanaphy@gmail.com>
2024-07-24 00:52:55 +0100

    reverting

---
+++ client/task_management_system/linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,21 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/diarmuid/snap/flutter/common/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/diarmuid/Documents/task_manager/client/task_management_system" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/diarmuid/snap/flutter/common/flutter"
+  "PROJECT_DIR=/home/diarmuid/Documents/task_manager/client/task_management_system"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9lZGQ4NTQ2MTE2NDU3YmRmMWM1YmRmYjEzZWNiOTQ2M2QyYmI1ZWQ0Lw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/diarmuid/Documents/task_manager/client/task_management_system/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/diarmuid/Documents/task_manager/client/task_management_system/lib/main.dart"
+)
